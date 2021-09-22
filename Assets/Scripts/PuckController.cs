9f776d72737f168f1bd70147d59a8be8b70e78e1
Monky216 <90779085+Monky216@users.noreply.github.com>
2021-09-22 10:02:55 -0400

    Started Video 6 (2:00)

---
+++ Assets/Scripts/PuckController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuckController : MonoBehaviour
+{
+    public int[] direction = { 0, 1, 2, 3 };
+    public int moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
