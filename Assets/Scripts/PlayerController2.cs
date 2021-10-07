73c7dabc47e322d559ee362d376c0db52b35b50f
Monky216 <90779085+Monky216@users.noreply.github.com>
2021-10-07 10:04:13 -0400

    Finish Video 8 (13:32)

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -9,6 +9,7 @@ public class PlayerController2 : MonoBehaviour
     public float yRange;
     public GameObject Puck;
     public GameObject Blocky;
+    public GameObject scoreText;
 
     // Start is called before the first frame update
     void Start()
@@ -24,10 +25,23 @@ public class PlayerController2 : MonoBehaviour
     void Update()
     {
         //Puck spawner
-        //Instantiate(Puck, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+        Instantiate(Puck, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
 
         //How many Pucks are on screen
         GameObject[] puckArray;
             puckArray = GameObject.FindGameObjectsWithTag("Puck");
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Blocky"))
+        {
+            Destroy(other.gameObject);
+            Instantiate(Blocky, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+
+            //Score
+            scoreText.GetComponent<ScoreKeeper>().UpdateScore();
+        }
+    }
+
 }
