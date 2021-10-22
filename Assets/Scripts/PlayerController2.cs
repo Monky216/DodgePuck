d6ae3a2a3945e340c9fa0103b30f46e1efe3c19e
Monky216 <90779085+Monky216@users.noreply.github.com>
2021-10-22 15:22:49 -0400

    A lot of SafeZone work

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -35,7 +35,8 @@ public class PlayerController2 : MonoBehaviour
             //level 2!
             Destroy(other.gameObject);
             Instantiate(Blocky, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
-            Instantiate(Puck, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+            //Instantiate(Puck, new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange)), Quaternion.identity);
+            scoreText.GetComponent<SafeZone>().OnTriggerEnter2D();
 
             //Score
             scoreText.GetComponent<ScoreKeeper>().UpdateScore();
